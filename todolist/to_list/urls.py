3d5041c4969da60cbbf6e6fd5f572979d31dc4cc
Carlos <carlinhos.slz@hotmail.com>
2023-11-15 12:08:42 -0300

    update CRUD

--- todolist/to_list/urls.py
+++ todolist/to_list/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from to_list.views import VerListaView, CriarTarefaView
+from to_list.views import VerListaView, CriarTarefaView,EliminarTarefaView, EditarTarefaView
 
 urlpatterns = [
     path("", VerListaView.as_view(), name="ver_lista"),
-    path('criar/', CriarTarefaView.as_View(), name='criar_tarefa'),
+    path('criar/', CriarTarefaView.as_view(), name='criar_tarefa'),
+    path('eliminar/<int:pk>/', EliminarTarefaView.as_view(), name='eliminar_tarefa'),
+    path('editar/<int:pk>/', EditarTarefaView.as_view(), name='editar_tarefa')
+
 ]
